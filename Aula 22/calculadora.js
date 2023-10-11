759bf38f9945ca84d8e4c1d1dafd7a225387d6b6
KauanGavinho <gavinhods@gmail.com>
2023-10-11 12:01:09 -0300

    Atividade da aula 22

---
+++ Aula 22/calculadora.js
@@ -0,0 +1,25 @@
+const PORT = 8080;
+const express = require("express");
+const app = express();
+const fs = require('fs');
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+res.render('form')
+})
+
+app.post('/app',(req, res) => {
+const data = {
+n1: req.body.n1,
+n2: req.body.n2,
+result: parseFloat(req.body.n1) + parseFloat(req.body.n2)
+}
+res.render('result', {data})
+})
+
+app.listen(PORT, () => {
+console.log(`server funcionando na porta ${PORT}`)
+})
